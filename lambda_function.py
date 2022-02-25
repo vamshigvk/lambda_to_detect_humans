2ce910a586c53d1aa7c3b5bdc467ac09c8693a9a
Your Name <vamshikrishnagvk5@gmail.com>
2022-02-25 15:17:45 +0530

    buildspec yolov5:latest

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,22 @@
+import os
+import json
+
 def lambda_handler(event, context):
+    if event:
+        file_obj = event["Records"][0]
+        bucketname = str(file_obj["s3"]["bucket"]["name"])
+        filename = str(file_obj["s3"]["object"]["key"])
+        
+        main(bucketname, filename)
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Document processed successfully!"),
+        }
 
-    main()
+    return {"statusCode": 500, "body": json.dumps("There is an issue!")}
 
-def main():
-    python3 detect.py -weights "model path" -source "img file" -view-img
-    return "hi from main"
+def main(bucketname,filename):
+    print('before triggering detect, filename: ', filename)
+    os.system("python3 detect.py -source "+ filename )
+    print('after triggering detect')
