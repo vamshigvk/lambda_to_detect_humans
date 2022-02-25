65a7d751602a7883f5b6ad69344c33eade95af1e
Your Name <vamshikrishnagvk5@gmail.com>
2022-02-25 22:40:22 +0530

    build added upload output

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,8 @@ import boto3
 
 s3 = boto3.client('s3')
 
+destination_bucketname = 'yolov5.output'
+
 def lambda_handler(event, context):
     if event:
         file_obj = event["Records"][0]
@@ -20,10 +22,31 @@ def lambda_handler(event, context):
     return {"statusCode": 500, "body": json.dumps("There is an issue!")}
 
 def main(bucketname,filename):
-    print('before triggering detect, filename: ', filename)
+    
+    print('start of main')
+
     try:
+        print('before downloading file from S3, filename: ', filename)
         s3.download_file(bucketname, filename, filename)
+        print('After downloading file from S3 ')
     except:
         print('image downloading failed from S3')
-    os.system("python3 detect.py -source "+ filename )
-    print('after triggering detect')
+
+
+    try:
+        print('before triggering detect')
+        os.system("python3 detect.py --source "+filename )
+        print('after triggering detect')
+    except:
+        print('exception while running detect')
+
+
+    try:
+        print('before saving output file to S3 ')
+        s3.download_file(destination_bucketname, 'runs/detect/exp/'+filename, filename)
+        print('after saving output file to S3')
+
+    except:
+        print('image downloading failed from S3')
+
+    print('end of main')
