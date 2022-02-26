f7b5e6a9afa8a0e87f24afe95a2da5c8119c5410
Your Name <vamshikrishnagvk5@gmail.com>
2022-02-26 22:45:41 +0530

    build yolo/ to ..

--- lambda_function.py
+++ lambda_function.py
@@ -21,14 +21,18 @@ def lambda_handler(event, context):
     path, filename = os.path.split(file_key_name)
     print('Key we are downloading is: ',filename)
     
-    print('before downloading file from S3, filename: ', filename)
+    print('before downloading file from S3, filename: at /tmp/', filename)
     bucket.download_file(file_key_name, "/tmp/" + filename)
 
+    print('inside this directory: ',os.getcwd())
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    print('contents in current directory:', files)
+
     print('before calling detect python file')
-    os.system("python3 yolov5/detect.py --source /tmp/"+ filename )
+    os.system("python3 detect.py --source /tmp/"+ filename )
 
     print('before uploading output file to destination S3 bucket')
-    s3.upload_file('yolov5/runs/detect/exp/'+filename, destination_bucketname, 'output_'+filename)
+    s3.upload_file('runs/detect/exp/'+filename, destination_bucketname, 'output_'+filename)
     print('end of main') 
 
     return {
