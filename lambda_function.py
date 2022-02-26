8027404fa352b5557310c77b8dfa0088209f19c8
Your Name <vamshikrishnagvk5@gmail.com>
2022-02-26 15:51:18 +0530

    build added /tmp/

--- lambda_function.py
+++ lambda_function.py
@@ -3,29 +3,36 @@ import json
 import boto3
 
 s3 = boto3.client('s3')
+s3_resource = boto3.resource('s3')
+
 
 destination_bucketname = 'yolov5.output'
 
 def lambda_handler(event, context):
-    if event:
-        file_obj = event["Records"][0]
-        bucketname = str(file_obj["s3"]["bucket"]["name"])
-        filename = str(file_obj["s3"]["object"]["key"])
-        
-        main(bucketname, filename)
-        
-        return {
-            "statusCode": 200,
-            "body": json.dumps("Document processed successfully using yolov5!"),
-        }
+    
+    print("Event :", event)
+    source_bucket_name = event['Records'][0]['s3']['bucket']['name']
+    print("Source bucket name is: ", source_bucket_name, "only")
 
-    return {"statusCode": 500, "body": json.dumps("There is an issue!")}
-
-def main(bucketname,filename):
-    os.system('chmod -R 777 yolov5/')
+    file_key_name = event['Records'][0]['s3']['object']['key']
+    print('File key name is: ', file_key_name, "only")
+    
+    bucket = s3_resource.Bucket(source_bucket_name)
+    path, filename = os.path.split(file_key_name)
+    print('Key we are downloading is: ',filename)
+    
     print('before downloading file from S3, filename: ', filename)
-    s3.download_file(bucketname, filename, 'yolov5/'+filename)
-    os.system("python3 yolov5/detect.py --source yolov5/"+ filename )
-    s3.upload_file(destination_bucketname, 'yolov5/runs/detect/exp/'+filename, filename)
-    print('end of main')
+    bucket.download_file(file_key_name, "/tmp/" + filename)
+
+    print('before calling detect python file')
+    os.system("python3 yolov5/detect.py --source /tmp/"+ filename )
+
+    print('before uploading output file to destination S3 bucket')
+    s3.upload_file('yolov5/runs/detect/exp/'+filename, destination_bucketname, 'output_'+filename)
+    print('end of main') 
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Document processed successfully using yolov5!"),
+    }
 
