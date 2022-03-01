64998b26e722f1199d9ac6013c07a12af2e32aee
Your Name <vamshikrishnagvk5@gmail.com>
2022-03-01 17:18:42 +0530

    build added path to output bucket and email

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,18 @@
 import os
 import json
 import boto3
+from decimal import Decimal
+import smtplib
+from email.message import EmailMessage
+from os import environ
+from datetime import date
 
 s3 = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
 
-destination_bucketname = 'yolov5.output'
+destination_bucketname = os.environ['destination_bucketname']
+
 
 def lambda_handler(event, context):
     
@@ -34,14 +40,49 @@ def lambda_handler(event, context):
     print('before uploading output file to destination S3 bucket')
     
     try:
-        s3.upload_file('/tmp/exp/'+filename, destination_bucketname, 'output_'+filename)
+        s3.upload_file('/tmp/exp/'+filename, destination_bucketname, path+'output_'+filename)
     except:
-        s3.upload_file('/tmp/exp2/'+filename, destination_bucketname, 'output_'+filename)
+        s3.upload_file('/tmp/exp2/'+filename, destination_bucketname, path+'output_'+filename)
     
-    print('end of main')
+    print('end of yolo processing and uploading output image to s3 bucket')
+
+    
+    smtp_mail = os.environ['from_mail']
+    from_mail = os.environ['from_mail']
+    to_mail = os.environ['to_mail']
+    smtp_mail_password = os.environ['password']
+    
+    print('before calling email function')
+    mail_user(smtp_mail, from_mail, to_mail, smtp_mail_password, path, filename)
+    print('after sending email')
 
     return {
         "statusCode": 200,
         "body": json.dumps("Document processed successfully using yolov5!"),
     }
 
+
+
+def mail_user(smtp_mail, from_mail, to_mail, smtp_mail_password, path, filename):
+
+    print('inside mail_user method')
+    subject = "Image from lambda, Name: " + path + " " + filename
+    header = """
+        <html>
+        <body>
+             <TABLE CELLPADDING=10 CELLSPACING=20>
+        <tr> """ + " <th style='text-align:centre'> Cam Image: " + "<img src=" + "/tmp/" + filename + "alt='failed to load image'> </th> </tr>"
+
+    mailer = ""
+        
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = from_mail
+    msg['To'] = to_mail
+    msg.set_content('Hi. \n Please find the image. \n Thank you.')
+    msg.add_alternative(header + mailer, subtype='html')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(smtp_mail, smtp_mail_password)
+        smtp.send_message(msg)
+    print('Done mailing the user')
